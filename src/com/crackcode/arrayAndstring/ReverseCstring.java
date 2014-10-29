dc9f6404bc139cd0ab4e251a472053a21f0cea2b
usakey <rdxiaolei@gmail.com>
2014-10-29 15:53:40 +0100

    implementation of first two exercises

--- src/com/crackcode/arrayAndstring/ReverseCstring.java
+++ src/com/crackcode/arrayAndstring/ReverseCstring.java
@@ -1,5 +1,31 @@
 package com.crackcode.arrayAndstring;
 
 public class ReverseCstring {
+  public static void main(String[] args){
+    String s = "abcde";
+    /*
+     * First approach: reverse function of StringBuffer
+     */
+    StringBuffer sb = new StringBuffer(s);
+    System.out.println("first result: " + s + "-->" + sb.reverse());
 
+    /*
+     *
+     */
+    System.out.println("second result: " + s + "-->" + ReverseCstring.stringReverse(s));
+
+  }
+  /*
+   * reverse the string NOT using StringBuffer
+   */
+  public static String stringReverse(String str){
+    String reverse = "";   
+    if(str != null){
+      for(int i=str.length()-1; i>=0; --i){
+        reverse = reverse + str.charAt(i);
+      }
+    }else 
+      System.out.println("String is NULL!");
+    return reverse;    
+  }
 }
--- src/com/crackcode/arrayAndstring/UniqueCharacter.java
+++ src/com/crackcode/arrayAndstring/UniqueCharacter.java
@@ -1,5 +1,42 @@
 package com.crackcode.arrayAndstring;
 
 public class UniqueCharacter {
+  public static void main(String[] args){
+    String s1 = "abcd";
+    String s2 = "abca";
 
+    System.out.println("s1 is unique?" + UniqueCharacter.isUnique(s1));
+    System.out.println("s2 is unique?" + UniqueCharacter.isUnique(s2));
+    
+    System.out.println("s1 is unique?" + UniqueCharacter.isUniqueChars(s1));
+    System.out.println("s2 is unique?" + UniqueCharacter.isUniqueChars(s2));
+    
+  }
+  /* time O(n), space O(n)
+   * assume char set is ASCII
+   */
+  public static boolean isUnique(String str){
+    boolean[] Char_set = new boolean[256];
+    for(int i=0; i<str.length(); i++){
+      int index = str.charAt(i);
+      if(Char_set[index])return false;
+      Char_set[index] = true;
+    }
+    return true;
+  }
+
+  /*
+   * reduce a little space by bit vector
+   * TO BE INVESTIGATED
+   */
+  public static boolean isUniqueChars(String str) {
+    int checker = 0;
+    for (int i = 0; i < str.length(); ++i) {
+      int val = str.charAt(i) - 'a';
+      if ((checker & (1 << val)) > 0) return false;
+        checker |= (1 << val);
+        System.out.println(checker);
+      }
+      return true;
+    }
 }
