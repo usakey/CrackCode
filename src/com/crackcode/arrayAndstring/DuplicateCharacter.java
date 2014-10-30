0a3e2d58b705f452202573caa0c9491b6c55482e
usakey <rdxiaolei@gmail.com>
2014-10-30 18:27:19 +0100

    test for git fetch

--- src/com/crackcode/arrayAndstring/DuplicateCharacter.java
+++ src/com/crackcode/arrayAndstring/DuplicateCharacter.java
@@ -10,6 +10,7 @@ package com.crackcode.arrayAndstring;
 public class DuplicateCharacter {
   public static void main(String[] args){
 
+  
   }
 
   public static void duplicateCharacterRemove(String str){
